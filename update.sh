d969019456b4cbedf099d5ea64b4a943c7d9d8c9
Les Aker <me@lesaker.org>
2015-05-20 21:08:26 -0400

    hacky update script

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 domain="https://raw.githubusercontent.com"
-path="halyard/halyard/master/script/kickstart"
 
-curl -s -o kickstart "$domain/$path"
+path="halyard/halyard/master/script/kickstart"
+curl -s -o kickstart/halyard "$domain/$path"
+
+path="halyard/goblin/master/meta/install"
+curl -s -o kickstart/goblin "$domain/$path"
 
